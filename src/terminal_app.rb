ba68753ef35024ed7635a91fa04db4ebd143d1b9
Ana Lazarevska <aplazarevska@gmail.com>
2021-05-21 13:26:15 +1000

    set the menu options to point to the correct variables

--- src/terminal_app.rb
+++ src/terminal_app.rb
@@ -24,16 +24,29 @@ welcome_message.each_char do |char|
     sleep 0.05
 end
 
-print "\n \n Press enter to proceed."
+print "\n Press enter to proceed."
 
 enter = gets
 
-
-
-
 menu_options = ["1. The Adventure of the Crooked Coder", "2. The Adventure of the Ruby Gem", "3. Personalised Story", "4. Exit"]
-# puts menu_options
-prompt.select("\n If you want to read a book choose option 1 or 2. If you want to personalise a book choose option 3.", menu_options)
-# menu_options.each do |title|
-#     p "#{title}"
-# end
+
+optionz = prompt.select("\n If you want to read a book choose option 1 or 2. If you want to personalise a book choose option 3.") do |title|
+    title.choice "1. The Adventure of the Crooked Coder"
+    title.choice "2. The Adventure of the Ruby Gem"
+    title.choice "3. Personalised Story"
+    title.choice "4. Exit"
+end
+
+story1 = "Here is story 1"
+story2 = "Here is story 2"
+personalised = "Your story"
+
+if optionz == "1. The Adventure of the Crooked Coder"
+    p story1
+elsif optionz == "2. The Adventure of the Ruby Gem"
+    p story2
+elsif optionz == "3. Personalised Story"
+    p personalised
+elsif optionz == "4. Exit" 
+    p "Thank you for reading!"
+end
