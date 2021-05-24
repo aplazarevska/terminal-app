62b2408a76030bc93107e2999f050b863f9c55c1
Ana Lazarevska <aplazarevska@gmail.com>
2021-05-24 20:39:35 +1000

    uncommented parts of the code

--- src/terminal_app.rb
+++ src/terminal_app.rb
@@ -14,22 +14,22 @@ require "tty-box"
 
 require 'colorize'
 
-# welcome = "Welcome \n reader."
-# welcome.art!
+welcome = "Welcome \n reader."
+welcome.art!
 
-# welcome.each_char do |char|
-#     putc char
-#     $stdout.flush
-#     sleep 0.01
-# end
+welcome.each_char do |char|
+    putc char
+    $stdout.flush
+    sleep 0.01
+end
 
-# welcome_message = "\n You are about to set on a reading adventure. \n Where the story will take you depends solely on you.\n"
+welcome_message = "\n You are about to set on a reading adventure. \n Where the story will take you depends solely on you.\n"
 
-# welcome_message.each_char do |char|
-#     putc char
-#     $stdout.flush
-#     sleep 0.05
-# end
+welcome_message.each_char do |char|
+    putc char
+    $stdout.flush
+    sleep 0.05
+end
 
 print "\n Press enter to proceed.".colorize(:red)
 
